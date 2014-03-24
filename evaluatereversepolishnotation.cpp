5e1ee8812b2a44105242aafae20190efdb5cc82b
qieka <2008shdjc@163.com>
2014-03-24 22:55:36 +0800

    right:using atoi

--- evaluatereversepolishnotation.cpp
+++ evaluatereversepolishnotation.cpp
@@ -1,3 +1,6 @@
+//no using atoi s.c_str()
+//int atoi(const char*nptr)
+//const char*c_str()
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -19,14 +22,9 @@ int main(){
 						if(tokens[i]=="/") numbers.push_back(b/a);
 					}
 					else{
-						string temp=tokens[i];
-						int isize=temp.size();
-						int c=0,flag=1;
-						for(int j=0;j<isize;++j){
-							if(temp[j]=='-')  flag=-1; 
-							else	c=c*10+(temp[j]-'0');
-						}
-						numbers.push_back(c*flag);
+						int c=0;
+						c=atoi(tokens[i].c_str());
+						numbers.push_back(c);
 					}
 				}
 				return numbers.back();
