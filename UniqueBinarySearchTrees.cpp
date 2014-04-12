dca52bda9ac7f65d3b2ad83cb84aa580081af0bc
qieka <2008shdjc@163.com>
2014-04-12 11:10:39 +0800

    right:not use recursion but i still donot know why pre versionis wrong

--- UniqueBinarySearchTrees.cpp
+++ UniqueBinarySearchTrees.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 class Solution {
 public:
-	    int numTrees(int n) {
+	    int numTrees1(int n) {
 		static vector<int> storage(n+1,0);
 		if(storage[n]!=0) return storage[n];
 		if(n==0){
@@ -21,10 +21,24 @@ public:
 		cout<<"storage"<<n<<"="<<storage[n]<<"\n";
 		return storage[n];
     }
+	int numTrees(int n){
+		vector<int> storage(n+1,0);
+		if(n==0) return 1;
+		if(n==1) return 1;
+		storage[0]=1;
+		storage[1]=1;
+		for(int i=2;i< n+1;++i)
+			for(int j=0;j<i;++j){
+				storage[i]+=storage[j]*storage[i-j-1];
+			}
+		cout<<"storage"<<n<<"="<<storage[n]<<"\n";
+		return storage[n];
+	}
 };
 int main(){
 	Solution a;
-	cout<<a.numTrees(99);
+	cout<<a.numTrees(6);
+	cout<<a.numTrees1(6);
 	return 0;
 }
 
